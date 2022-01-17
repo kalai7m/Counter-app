a88af8dba32071f67b0a9f7d518acb836b9ad554
kalai7m <kalaivani7m@gmail.com>
2022-01-17 17:51:38 +0530

    rendering ele conditionaly'

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,25 +3,23 @@ import React, { Component } from 'react';
 class Counter extends Component {
    
     state = {
-        count: 0
+        count: 0,
+        tags: []
+    }
+    renderTags() {
+        //conditional rendering
+        const {tags} =this.state;
+        if(tags.length === 0)
+            return <p>No Tags in list</p>
+        return <ul>{tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
     }
     render() {
         return (
             <React.Fragment>
-                <span className={this.getBadgeClass()}>{this.formatCount()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
+                {this.state.tags.length===0 && "Please create new tag"}
+                {this.renderTags()}
             </React.Fragment>
         );
-    }
-    getBadgeClass() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? 'warning' : 'primary';
-        return classes;
-    }
-
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
     }
 }
  
